a3b2dc0fa1bd89a8d91e3f13822748145fc608c9
Julien LEFEVRE <julien.lefevre@twenga.com>
2014-06-06 19:28:44 +0200

    adding argparse

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="qi",
-    version="0.1",
+    version="0.1.1",
     author="Julien LEFEVRE",
     author_email="julien.lefevr@gmail.com",
     packages=['qi'],
