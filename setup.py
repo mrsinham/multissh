3b80d79a73076afe9389cf6152feba8da166b732
Julien LEFEVRE <julien.lefevre@twenga.com>
2014-06-09 15:04:38 +0200

    description added

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     name="qi",
     version="0.1.1",
     author="Julien LEFEVRE",
+    description="A tool to execute same request on multiple servers in parallel. Needs to be autorized by keys.",
     author_email="julien.lefevr@gmail.com",
     packages=['qi'],
     py_modules=['main', 'config', 'command'],
